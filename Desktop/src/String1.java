66d4a73b24d9b7f3258bbb63da97740129c0b147
Ranathunga R.G.R.L. <116500345+lakshanravi@users.noreply.github.com>
2025-03-22 02:20:00 +0530

    Delete Desktop/src directory

--- Desktop/src/String1.java
+++
@@ -1,7 +0,0 @@
-public class String1 {
-    public static void main(String[] args) {
-        String[] ab={"kamal","nimal"};
-        System.out.println(ab[1]);
-        System.out.println(ab.length);
-    }
-}
